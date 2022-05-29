dda5f03dd6f642dd13d7d1c35449b00d03f272b6
zuoyu2014 <zuoyu2000@hotmail.com>
2022-05-29 19:51:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# mbit
+
+Extension for own module
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ main.ts
@@ -0,0 +1,143 @@
+﻿/*
+Copyright (C): None
+modified from zy
+*/
+
+
+
+//% color="#C814B8" weight=25 icon="\uf1d4"
+namespace zybit {
+    export enum enColor {
+        //% blockId="OFF" block="灭"
+        OFF = 0,
+        //% blockId="Red" block="红色"
+        Red,
+        //% blockId="Green" block="绿色"
+        Green,
+        //% blockId="Blue" block="蓝色"
+        Blue,
+        //% blockId="White" block="白色"
+        White,
+        //% blockId="Cyan" block="青色"
+        Cyan,
+        //% blockId="Pinkish" block="品红"
+        Pinkish,
+        //% blockId="Yellow" block="黄色"
+        Yellow,
+    }
+    export enum enLEDStatus {
+        //% blockId="OFF" block="灭"
+        OFF = 0,
+        //% blockId="ON" block="亮"
+        ON =1
+    }
+
+    //% blockId=zybit_DigitalLED block="DigitalLED|pin %pin|value %value"
+    //% weight=5
+    //% blockGap=8
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
+    export function DigitalLED(pin: DigitalPin, value: enLEDStatus): void {
+        pins.digitalWritePin(pin, value);
+
+    }
+
+    //% blockId=zybit_AnalogLED block="AnalogLED|pin %pin|value %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+    //% value.min=0 value.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=2
+    export function AnalogLED(pin: AnalogPin, value: number): void {
+        pins.analogWritePin(pin, value * 1024 / 256);
+    }
+
+    //% blockId=zybit_BreathLED block="BreathLED|pin %pin"
+    //% weight=3
+    //% blockGap=8
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=3
+    export function BreathLED(pin: AnalogPin): void {
+        for (let i: number = 0; i < 1023; i++) {
+            pins.analogWritePin(pin, i);
+            control.waitMicros(1000);
+        }
+        basic.pause(10);
+        for (let i: number = 1023; i > 0; i--) {
+            pins.analogWritePin(pin, i);
+            control.waitMicros(1000);
+        }
+    }
+
+    //% blockId=zybit_AnalogRGB block="AnalogRGB|pin1 %pin1|pin2 %pin2|pin3 %pin3|value1 %value1|value2 %value2|value3 %value3"
+    //% weight=2
+    //% blockGap=8
+    //% color="#C814B8"
+    //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function AnalogRGB(pin1: AnalogPin, pin2: AnalogPin, pin3: AnalogPin, value1: number, value2: number, value3: number): void {
+
+        pins.analogWritePin(pin1, value1 * 1024 / 256);
+        pins.analogWritePin(pin2, value2 * 1024 / 256);
+        pins.analogWritePin(pin3, value3 * 1024 / 256);
+
+    }
+	
+    //% blockId=zybit_DigitalRGB block="DigitalRGB|pin1 %pin1|pin2 %pin2|pin3 %pin3|value %value"
+    //% weight=1
+    //% blockGap=8
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function DigitalRGB(pin1: DigitalPin, pin2: DigitalPin, pin3: DigitalPin, value: enColor): void {
+        switch (value) {
+            case enColor.OFF: {
+                pins.digitalWritePin(pin1, 0);
+                pins.digitalWritePin(pin2, 0);
+                pins.digitalWritePin(pin3, 0);
+                break;
+            }
+            case enColor.Red: {
+                pins.digitalWritePin(pin1, 1);
+                pins.digitalWritePin(pin2, 0);
+                pins.digitalWritePin(pin3, 0);
+                break;
+            }
+            case enColor.Green: {
+                pins.digitalWritePin(pin1, 0);
+                pins.digitalWritePin(pin2, 1);
+                pins.digitalWritePin(pin3, 0);
+                break;
+            }
+            case enColor.Blue: {
+                pins.digitalWritePin(pin1, 0);
+                pins.digitalWritePin(pin2, 0);
+                pins.digitalWritePin(pin3, 1);
+                break;
+            }
+            case enColor.White: {
+                pins.digitalWritePin(pin1, 1);
+                pins.digitalWritePin(pin2, 1);
+                pins.digitalWritePin(pin3, 1);
+                break;
+            }
+            case enColor.Cyan: {
+                pins.digitalWritePin(pin1, 0);
+                pins.digitalWritePin(pin2, 1);
+                pins.digitalWritePin(pin3, 1);
+                break;
+            }
+            case enColor.Pinkish: {
+                pins.digitalWritePin(pin1, 1);
+                pins.digitalWritePin(pin2, 0);
+                pins.digitalWritePin(pin3, 1);
+                break;
+            }
+            case enColor.Yellow: {
+                pins.digitalWritePin(pin1, 1);
+                pins.digitalWritePin(pin2, 1);
+                pins.digitalWritePin(pin3, 0);
+                break;
+            }
+        }
+    }
+}
