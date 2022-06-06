87f5cb14fb6348ab0a3c7f654aa07a052ccb405b
zuoyu2014 <zuoyu2000@hotmail.com>
2022-06-07 06:53:08 +0800

    add input

---
+++ main_input.ts
@@ -0,0 +1,114 @@
+﻿/*
+Copyright (C): None
+modified from zy
+*/
+
+
+//% color="#808080" weight=23 icon="\uf11c"
+namespace zybit_input {
+
+    export enum enRocker {
+        //% blockId="Nostate" block="无"
+        Nostate = 0,
+        //% blockId="Up" block="上"
+        Up,
+        //% blockId="Down" block="下"
+        Down,
+        //% blockId="Left" block="左"
+        Left,
+        //% blockId="Right" block="右"
+        Right,
+        //% blockId="Press" block="按下"
+        Press
+    }
+
+    export enum enTouch {
+        //% blockId="NoTouch" block="未触摸"
+        NoTouch = 0,
+        //% blockId="Touch" block="触摸"
+        Touch = 1
+    }
+    export enum enButton {
+        //% blockId="Press" block="按下"
+        Press = 0,
+        //% blockId="Realse" block="松开"
+        Realse = 1
+    }
+
+    //% blockId=mbit_TouchPad block="TouchPad|pin %pin|value %value"
+    //% weight=100
+    //% blockGap=10
+    //% color="#808080"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function TouchPad(pin: DigitalPin, value: enTouch): boolean {
+
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    
+    //% blockId=mbit_Rocker block="Rocker|VRX %pin1|VRY %pin2|SW %pin3|value %value"
+    //% weight=100
+    //% blockGap=10
+    //% color="#808080"
+    export function Rocker(pin1: AnalogPin, pin2: AnalogPin, pin3: DigitalPin, value: enRocker): boolean {
+
+        pins.setPull(pin3, PinPullMode.PullUp);
+        let x = pins.analogReadPin(pin1);
+        let y = pins.analogReadPin(pin2);
+        let z = pins.digitalReadPin(pin3);
+        let now_state = enRocker.Nostate;
+
+        if (x < 100) // 上
+        {
+
+            now_state = enRocker.Up;
+
+        }
+        else if (x > 700) //
+        {
+
+            now_state = enRocker.Down;
+        }
+        else  // 左右
+        {
+            if (y < 100) //右
+            {
+                now_state = enRocker.Right;
+            }
+            else if (y > 700) //左
+            {
+                now_state = enRocker.Left;
+            }
+        }
+        if (z == 0)
+            now_state = enRocker.Press;
+        if (now_state == value)
+            return true;
+        else
+            return false;
+
+    }
+
+    //% blockId=mbit_Button block="Button|pin %pin|value %value"
+    //% weight=100
+    //% blockGap=10
+    //% color="#808080"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Button(pin: DigitalPin, value: enButton): boolean {
+
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }  
+}
